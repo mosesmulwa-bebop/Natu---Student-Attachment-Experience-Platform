310fdff5aed3ed5d141176e8d4cd7b14f90f5ae9
mosesmulwa-bebop <54888024+mosesmulwa-bebop@users.noreply.github.com>
2021-09-11 17:28:24 +0300

    Updated config file

--- attachment_config.php
+++ attachment_config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'mulwacok_admin');
-define('DB_PASSWORD', 'MG*PoKHri{mh');
-define('DB_NAME', 'mulwacok_attachmentdb');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
